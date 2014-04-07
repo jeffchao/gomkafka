7ca4d08244dc6f5b6c90dee5500d6c44d8262965
Jeff Chao <jeffchao@me.com>
2014-04-07 14:49:26 -0700

    Add rsyslog listener. Add kafka producer.
    Writing to kafka WIP.

--- gomkafka/gomkafka.go
+++ gomkafka/gomkafka.go
@@ -1,1 +1,41 @@
 package gomkafka
+
+import (
+	kafka "github.com/Shopify/sarama"
+	"log"
+	"time"
+)
+
+type KafkaConfig struct {
+	ClientId string
+	Hosts    []string
+	Topic    string
+}
+
+// Initialize a kafka client and producer based off of KafkaConfig.
+func Gomkafka() (*kafka.Client, *kafka.Producer, error) {
+	config := KafkaConfig{
+		"client_id",
+		[]string{"localhost:9092"},
+		"monitoring",
+	}
+
+	log.Println("initializing Kafka client...")
+	client, err := kafka.NewClient(config.ClientId, config.Hosts, &kafka.ClientConfig{MetadataRetries: 1, WaitForElection: 250 * time.Millisecond})
+	if err != nil {
+		panic(err)
+	} else {
+		log.Println("kafka client connected")
+	}
+
+	log.Println("initializing Kafka producer...")
+	producer, err := kafka.NewProducer(client, &kafka.ProducerConfig{RequiredAcks: kafka.WaitForLocal, MaxBufferedBytes: 1, MaxBufferTime: 1})
+	if err != nil {
+		panic(err)
+	}
+
+	return client, producer, nil
+}
+
+func Receive() {
+}
--- main.go
+++ main.go
@@ -1,4 +1,62 @@
 package main
 
+import (
+	"fmt"
+	"github.com/jeffchao/gomkafka/gomkafka"
+	"log"
+  "time"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func run() {
+  go handleSignals()
+  work()
+}
+
+func work() {
+	log.Println("waiting for rsyslog input...")
+	rsyslog := ""
+
+	for {
+		_, err := fmt.Scanf("%s\n", &rsyslog)
+
+		if err != nil {
+			return
+		}
+
+		fmt.Println(rsyslog)
+    time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func handleSignals() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGUSR1, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt)
+
+	for s := range signals {
+		switch s {
+		case syscall.SIGINT, syscall.SIGUSR1, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt:
+			quit()
+		}
+	}
+}
+
+func quit() {
+	log.Println("Waiting for cleanup...")
+  // Perform any necessary cleanup here.
+	log.Println("Exiting")
+	os.Exit(1)
+}
+
 func main() {
+	client, producer, err := gomkafka.Gomkafka()
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+	defer producer.Close()
+
+  run()
 }

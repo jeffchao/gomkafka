5ce0f75a12d5df942144deb1dcaa35dd7ad4c383
Jeff Chao <jeffchao@me.com>
2014-05-05 11:41:31 -0700

    Add coverage around empty messages.

--- main_test.go
+++ main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"github.com/Shopify/sarama"
+	"os"
+	"testing"
+)
+
+func TestInitConfig(t *testing.T) {
+	os.Args = nil
+	os.Args = append(os.Args, "gomkafka")
+	os.Args = append(os.Args, "client_id")
+	os.Args = append(os.Args, "localhost:8080")
+	os.Args = append(os.Args, "foo_topic")
+	_, err := initConfig()
+	if err != nil {
+		t.Errorf("error: failed to initialize gomkafka config.")
+	}
+}
+
+func TestWork(t *testing.T) {
+	mb := sarama.NewMockBroker(t, 1)
+	mb.Returns(new(sarama.MetadataResponse))
+
+	os.Args = nil
+	os.Args = append(os.Args, "gomkafka")
+	os.Args = append(os.Args, "client_id")
+	os.Args = append(os.Args, mb.Addr())
+	os.Args = append(os.Args, "foo_topic")
+
+	err := work()
+	// Test empty input case.
+	if err == nil {
+		t.Errorf("error. %+v", err)
+	}
+}
+
+func assertEquals(t *testing.T, expected interface{}, actual interface{}) {
+	if expected != actual {
+		t.Errorf("error. got: %d, expected: %d.", actual, expected)
+	}
+}
